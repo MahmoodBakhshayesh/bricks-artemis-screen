4ae40c410f6d553187a6cf7d7978370cceda4085
MahmoodBKH <s.ma.bakhshayesh@gmail.com>
2024-11-28 13:54:22 +0330

    version: 0.5.0+3

--- __brick__/{{screen_name.snakeCase()}}/data_sources/{{screen_name.snakeCase()}}_local_ds.dart
+++ __brick__/{{screen_name.snakeCase()}}/data_sources/{{screen_name.snakeCase()}}_local_ds.dart
@@ -3,7 +3,7 @@ import '../../../initialize.dart';
 import '../interfaces/{{screen_name.snakeCase()}}_data_source_interface.dart';
 
 class {{screen_name.pascalCase()}}LocalDataSource implements {{screen_name.pascalCase()}}DataSourceInterface {
-  final LocalDataSource localDataSource = getIt<LocalDataBase>();
+  final LocalDataBase localDataSource = getIt<LocalDataBase>();
   {{screen_name.pascalCase()}}LocalDataSource();
 
 
